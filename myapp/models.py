32a051537e4f6be390cb6d0126518a612a068e30
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-04-02 14:03:30 +0900

    データベース

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+   title = models.CharField(max_length=255)
+   slug = models.SlugField()
+   intro = models.TextField()
+   body = models.TextField()
+   posted_date = models.DateTimeField(auto_now_add=True)
