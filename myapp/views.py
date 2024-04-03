7a10a241e9758efc8c50a0716793287e0e5bfbae
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-04-03 15:37:09 +0900

    コメント

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from myapp.forms import CommentForm
 from .models import Post
 
 def frontpage(request):
@@ -8,4 +9,20 @@ def frontpage(request):
 
 def post_detail(request, slug):
  post = Post.objects.get(slug=slug)
- return render(request, "myapp/post_detail.html", {"post": post})  
+
+ if request.method == 'POST':
+     form = CommentForm(request.POST)
+
+     if form.is_valid():
+      comment = form.save(commit=False)
+      comment.post = post
+      comment.save()
+
+      return redirect("post_detail", slug=post.slug)
+
+ else:
+     form = CommentForm()
+
+
+
+ return render(request, "myapp/post_detail.html", {"post": post, "form": form})  
